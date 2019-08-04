6933b50bfd13009609acf7ad5ea5d65233197687
Talha <talhaayar53@gmail.com>
2019-08-04 13:37:05 +0300

    First commit

--- tapToShare/ViewController.swift
+++ tapToShare/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var countries = ["brazil","china","korea","japan","spain","germany","uk","usa"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+        cell.imageView?.image = UIImage(named: countries[indexPath.row])
+        cell.textLabel?.text = countries[indexPath.row].uppercased()
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let image = UIImage(named: countries[indexPath.row])!.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        present(vc, animated: true)
+    }
+    
+    
+}
